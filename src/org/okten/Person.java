d51d516f33efa4539d2e87496968cacea1e60d51
EOkten <eduardokten@gmail.com>
2024-12-13 21:05:57 +0200

    lesson 5

--- src/org/okten/Person.java
+++ src/org/okten/Person.java
@@ -1,13 +1,19 @@
-package org.okten.jcfdemo;
+package org.okten;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class Person {
 
+    private int id;
+
     private String name;
 
     private int age;
+
+    private List<String> hobbies;
 }
